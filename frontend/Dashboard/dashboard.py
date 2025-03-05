3d84cc3dbc5c65ba1cfd22af95f38809c14a21a3
iagonegrao <iagocarboni.n@gmail.com>
2025-03-04 21:48:59 -0300

    Organização

--- frontend/Dashboard/dashboard.py
+++ frontend/Dashboard/dashboard.py
@@ -1,21 +1,27 @@
+import pandas as pd
+from sqlalchemy import create_engine
 import dash
 from dash import dcc, html
-import psycopg2
-import pandas as pd
+import warnings
 
-DATABASE_CONFIG = {
-    'dbname': 'monitoramento_dados',
-    'user': 'postgres',
-    'password': '191010',
-    'host': 'localhost'
-}
+# Suprimir avisos (opcional)
+warnings.filterwarnings("ignore", category=UserWarning)
 
+# Configurações do banco de dados
+DATABASE_URL = "postgresql+psycopg2://postgres:191010@localhost/dados_governo"
+
+# Cria uma conexão com o banco de dados usando SQLAlchemy
+engine = create_engine(DATABASE_URL)
+
+# Usa um context manager para garantir que a conexão seja fechada corretamente
+with engine.connect() as connection:
+    # Lê os dados da tabela data_monitoring
+    df = pd.read_sql_query("SELECT * FROM br_mapbiomas_alert", connection)
+
+# Inicializa o aplicativo Dash
 app = dash.Dash(__name__)
 
-conn = psycopg2.connect(**DATABASE_CONFIG)
-df = pd.read_sql_query("SELECT * FROM data_monitoring", conn)
-conn.close()
-
+# Layout do dashboard
 app.layout = html.Div(children=[
     html.H1(children='Dashboard de Dados Governamentais'),
 
@@ -23,7 +29,7 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure={
             'data': [
-                {'x': df['created_date'], 'y': df['update_date'], 'type': 'bar', 'name': 'Atualizações'},
+                {'x': df['DTPUBLI'], 'y': df['DTPUBLI'], 'type': 'bar', 'name': 'Atualizações'},
             ],
             'layout': {
                 'title': 'Histórico de Atualizações'
@@ -32,5 +38,6 @@ app.layout = html.Div(children=[
     )
 ])
 
+# Executa o servidor do dashboard
 if __name__ == '__main__':
     app.run_server(debug=True)
