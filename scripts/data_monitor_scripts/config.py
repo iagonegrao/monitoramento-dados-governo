fc1e5ca1d3668d675d43ebe49708b6d91eb78b06
iagonegrao <iagocarboni.n@gmail.com>
2025-03-09 11:42:28 -0300

    atualização de config

---
+++ scripts/data_monitor_scripts/config.py
@@ -0,0 +1,7 @@
+DATABASE_CONFIG = {
+    'dbname': 'dados_governo',
+    'user': 'postgres',
+    'password': '191010',  # Substitua pela senha que você definiu durante a instalação
+    'host': 'localhost',
+    'port': '5432'
+}
