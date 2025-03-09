be4f764a8a3e28f019f9fc6ad98d7b2a75e4dff4
iagonegrao <iagocarboni.n@gmail.com>
2025-03-09 12:44:45 -0300

    removendo comentario

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 DATABASE_CONFIG = {
     'dbname': 'dados_governo',
     'user': 'postgres',
-    'password': '191010',  # Substitua pela senha que você definiu durante a instalação
+    'password': '191010',
     'host': 'localhost',
     'port': '5432'
 }
